46ca35d17440728ed31f57186be89e54acd9febe
Gbemileke Ayodele <onabajogbemileke37@gmail.com>
2024-06-21 10:37:05 -0700

    progress

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,30 @@
 /*
- * File: 2-print_alphabet.c
+ * File: 9-print_comb.c
  * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase.
+ * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char letter;
+	int num;
 
-    for (letter = 'a'; letter <= 'z'; letter++)
-        putchar(letter);
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		
+		putchar(',');
+		putchar(' ');
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
-
