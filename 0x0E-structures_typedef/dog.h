ab935fabcfc4611dfe1244ab2bda587f2658d128
GbemilekeAyo <onabajogbemileke37@gmail.com>
2024-07-24 00:00:39 +0100

    completed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,14 +2,14 @@
 #define DOG
 
 /**
- *  * struct dog - dog class
- *  *
- *  * @name: Name of the dog
- *  *
- *  * @age: Age of the dog
-    *
- *  * @owner: Owner of the dog
- *  */
+ * struct dog - dog class
+ *  
+ * @name: Name of the dog
+ *  
+ * @age: Age of the dog
+ *
+ * @owner: Owner of the dog
+ */
 
 struct dog
 {
