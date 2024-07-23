51c48fcd59930d4388e078b82bbb3c9e6ab04aff
GbemilekeAyo <onabajogbemileke37@gmail.com>
2024-07-23 22:48:56 +0100

    completed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,12 @@
-#incluse <stdio.h>
+#include <stdio.h>
 /**
- *main -prints the name of the file
- *
- *Return: always 0
- */
+ *  *main - prints the name of the file
+ *   *
+ *    *Return: always 0
+ *    */
 
 int main(void)
 {
-             printf("%\n",_FILE_);
-	    return (0);
-}  
+		printf("%s\n", __FILE__);
+			return (0);
+}
